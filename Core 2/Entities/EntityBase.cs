b575bf006e4482f7883142f1251aff82a5fe0cc6
Alex75 <alessandro.piccione.75@gmail.com>
2016-03-28 23:20:52 +0200

    Create classic Library project. The NuGet library is yet in beta and doesn't work.

---
+++ Core 2/Entities/EntityBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace monei.Core.Entities
+{
+    public abstract class EntityBase<TKey>
+    {
+        public EntityBase()
+        {
+            CreationDate = DateTime.UtcNow;
+            LastModificationDate = null;
+        }
+
+        public virtual TKey Id { get; set; }
+
+        public virtual DateTime CreationDate { get; set; }
+        public virtual DateTime? LastModificationDate { get; set; }
+        //public virtual Account CreationAccount { get; set; }
+        //public virtual Account LastUpdateAccount { get; set; }
+    }
+
+}
