ee3fce2a18ad62932803a9af021aec5f9a5c95f0
Alex75 <alessandro.piccione.75@gmail.com>
2016-10-02 23:28:00 +0100

    Set description and dynamic version in AssemblyInfo file (ignored in this kind of project).

--- monei.Core/Properties/AssemblyInfo.cs
+++ monei.Core/Properties/AssemblyInfo.cs
@@ -6,13 +6,13 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("monei.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Core library for monei application.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("monei.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("en-US")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -22,4 +22,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a39cc2f9-fe55-40c2-92e5-64ff3a00fcbe")]
 
-
+/*
+[assembly: AssemblyVersion("1.0.1.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+*/
